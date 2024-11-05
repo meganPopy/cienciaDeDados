379ff645703e7a63abc0fc41c4e4b01647815313
Tânia <106488757+meganPopy@users.noreply.github.com>
2024-11-05 20:03:32 -0300

    a

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,14 @@ const url= 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoG
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasMundo = (dados.total_pessoas_mundo / 1e9)
+    const pessoasComAcessoAEducacao = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
+    const horas = parseInt(dados.tempo_medio_dia_estudando)
+    const minutos = Math.round((dados.tempo_medio_dia_estudando - horas) * 60)
+    const pessoasComEducacaoSuperior = (dados.total_pessoas_com_educacao_superior / 1e9)
+
+   
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de <span>${dados.total_pessoas_mundo}</span> de pessoas e que o total de pessoas com acesso à educação é de <span>${dados.total_pessoas_com_acesso_a_educacao}.</span> O tempo médio gasto por dia estudando é de <span>${dados.tempo_medio_dia_estudando}</span> horas e o total de pessoas com educação superior é de <span>${dados.total_pessoas_com_educacao_superior}.</span> Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?.`
@@ -12,9 +19,5 @@ async function vizualizarInformacoesGlobais() {
 }
 vizualizarInformacoesGlobais() 
 
-const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-const horas = parseInt(dados.tempo_medio)
-const minutos = Math.round((dados.tempo_medio - horas) * 60)
 
 
