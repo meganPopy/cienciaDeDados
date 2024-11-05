34eed164c3d5ac86a92ce7db87abd2c205b8f0b9
Tânia <106488757+meganPopy@users.noreply.github.com>
2024-11-05 20:00:01 -0300

    a

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -12,3 +12,9 @@ async function vizualizarInformacoesGlobais() {
 }
 vizualizarInformacoesGlobais() 
 
+const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+const horas = parseInt(dados.tempo_medio)
+const minutos = Math.round((dados.tempo_medio - horas) * 60)
+
+
