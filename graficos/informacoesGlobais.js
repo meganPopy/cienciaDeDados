394b98ee4f04c16e7540dd4e85a76b00a615a431
Tânia <106488757+meganPopy@users.noreply.github.com>
2024-11-17 09:02:20 -0300

    a

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -14,7 +14,7 @@ async function vizualizarInformacoesGlobais() {
    
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de <span>${pessoasMundo}</span> bilhões de pessoas e que o total de pessoas com acesso à educação é de <span>${pessoasComAcessoAEducacao}.</span> O tempo médio gasto por dia estudando é de <span>${horas}</span> horas e <span>${minutos} minutos</span> conectadas.O total de pessoas com educação superior é de <span>${pessoasComEducacaoSuperior}.</span> Isso significa que aproximadamente <span>${percentual}%</span> de pessoas tem acesso à Educação.Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?.`
+    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de <span>${pessoasMundo}</span> bilhões de pessoas e que o total de pessoas com acesso à educação é de <span>${pessoasComAcessoAEducacao}.</span> bilhões de pessoas. O tempo médio gasto por dia estudando é de <span>${horas}</span> horas e <span>${minutos} minutos</span> conectadas.O total de pessoas com educação superior é de <span>${pessoasComEducacaoSuperior}.</span> Isso significa que aproximadamente <span>${percentual}%</span> de pessoas tem acesso à Educação.Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
